4043499e2fb8af24c154ae8f04cc12a6f57cdf7f
Wendelsena <wendel4019@gmail.com>
2024-04-22 18:04:04 -0300

    OrderStatus enum

---
+++ src/main/java/com/polieduca/br/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.polieduca.br.entities.enums;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	// percorre todos os OrdersStatus e retorna o valor igual
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code!");
+	}
+	
+}
